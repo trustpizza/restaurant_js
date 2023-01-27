e92c7b195a0ce2f48d0f445d622af97b0e4cf0fc
trustpizza <maxelolsson@gmail.com>
2023-01-27 12:00:11 -0600

    Create Header and add background images

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import _ from 'lodash';
 import './style.css';
 import Blueberry from "./photos/blueberry.png";
+import Stars from "./photos/stars.jpg";
 
-function test() {
-    const ele = document.getElementById("container")
-    console.log(ele);
-    ele.innerHTML = 'Hi'
-    return ele;
-}
-
-test();
