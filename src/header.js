add89384205dc5cc0f4bf09890e5af9c7bf29971
trustpizza <maxelolsson@gmail.com>
2023-01-29 10:22:36 -0600

    Finish for night

--- src/header.js
+++ src/header.js
@@ -5,9 +5,14 @@ const header = () => {
     document.body.appendChild(header);
 
     const container = document.createElement("div");
-    container.classList.add("background", "text-center", "bg-gray-50", "text-gray-800", "py-20", "px-6", "bg-top");
-    container.style.backgroundImage = Background;
-    header.prepend(container);
+    container.classList.add("background", "text-center", "bg-blue-50", "text-gray-800", "py-20", "px-6", "bg-top");
+    
+    const title = document.createElement("h1");
+    title.textContent = "The Blueberry Bodega";
+    title.classList.add("text-6xl", "font-bold", "mt-0", "mb-6", "text-black");
+    container.append(title);
+
+    header.append(container);
 
 };
 
