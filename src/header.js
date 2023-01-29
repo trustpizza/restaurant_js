62ef287119207f95e1f496f062b9267a0f9f3340
trustpizza <maxelolsson@gmail.com>
2023-01-28 22:58:28 -0600

    Begin to migrate header

---
+++ src/header.js
@@ -0,0 +1,11 @@
+import Background from "";
+
+const header = () => {
+    const header = document.querySelector("header");
+
+    const container = document.createElement("div");
+    container.classList.add("text-center", "bg-gray-50", "text-gray-800", "py-20", "px-6", "bg-top");
+    
+};
+
+export { header }; 
