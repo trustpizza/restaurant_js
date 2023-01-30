cddbf8e6fc7ae9779448f1b3b41e4b1a9a2ee89e
trustpizza <maxelolsson@gmail.com>
2023-01-29 18:16:37 -0600

    Finish divFactory, requires fixing which div it is appended to

--- src/page-factory.js
+++ src/page-factory.js
@@ -1,13 +1,19 @@
-const body = document.querySelector("#body");
+const body = document.querySelector("#main");
 
-const pageFactory = (count, ...heights) => {
+const pageFactory = (count) => {
 
 };
 
-const divFactory = (height) {
+const divFactory = () => {
     let outerDiv = document.createElement("div");
     let innerDiv = document.createElement("div");
 
     //Set outerdiv octagon 
     outerDiv.classList.add("bg-blue-400", "min-h-fit", "md:w-1/2", "md:outer-octagon", "p-1", "py-2", "flex", "items-center", "justify-center", "w-11/12", "rounded");
-}
+
+    innerDiv.classList.add("flex", "flex-col", "items-center", "bg-blue-200", "box-border", "min-h-fit", "w-11/12", "md:inner-octagon", "md:p-5", "gap-2", "rounded");
+
+    outerDiv.appendChild(innerDiv);
+    body.appendChild(outerDiv);
+};
+
