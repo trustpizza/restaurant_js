f3c2677301ae84ee38b2ca03b2a8c95bdc75b4d9
trustpizza <maxelolsson@gmail.com>
2023-01-30 21:19:02 -0600

    Change order of elements

--- src/home-page.js
+++ src/home-page.js
@@ -2,8 +2,7 @@ import { pageFactory, divFactory } from './page-factory';
 import { header } from "./header";
 import _ from 'lodash';
 
-
-
+// Creates Header
 const HEADERBODY = header().container;
 const CONTAINER = document.getElementById("container");
 const HEADER = document.querySelector("header");
@@ -11,15 +10,25 @@ HEADER.appendChild(HEADERBODY);
 
 // Body element to which ALL items will be added
 const BODY = document.getElementById("body");
-// Building the home page structure
+// Building the home page structure/shell items
 const HOME = pageFactory("main", 2).main;
-// Building first paragraph
-let page = []
+// Building first paragraph out to fit the first shell
 const SECTION1 = HOME.children[0].firstChild
-let title1 = document.createElement("h1");
-title1.textContent = "Hours of Operation"
-title1.classList.add("pl-4", "text-2xl", "font-medium", "underline");
+let title1 = document.createElement("h2");
+title1.textContent = "A Little About Us";
+title1.classList.add("text-2xl", "font-medium", "underline");
 SECTION1.appendChild(title1);
+
+let p1 = document.createElement("p");
+p1.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut accumsan lacus. Praesent pulvinar enim sed justo laoreet fermentum. Curabitur rhoncus ligula id euismod fringilla. Suspendisse consequat convallis eros, non pulvinar arcu vulputate et. Suspendisse suscipit erat ut ultrices blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut a ex suscipit, fringilla lacus ac, venenatis justo. Sed sagittis massa nec arcu egestas, at aliquet tortor consectetur."
+SECTION1.appendChild(p1);
+
+// Second Section
+const SECTION2 = HOME.children[1].firstChild
+let title2 = document.createElement("h1");
+title2.textContent = "Hours of Operation"
+title2.classList.add("pl-4", "text-2xl", "font-medium", "underline");
+SECTION2.appendChild(title2);
 
 // Add all the days (there has to be a better way!)
 const paragraphFactory = (text) => {
@@ -33,13 +42,7 @@ const paragraphFactory = (text) => {
 let days = ["Sunday: 8am - 4pm", "Monday: 6am - 4pm", "Tuesday: 6am - 4pm", "Wednesday: 6am - 4pm", "Thursday: 6am - 4pm", "Friday: Gone Hiking", "Saturday: Gone Fishing"]
 days.forEach(day => {
     let p = paragraphFactory(day).p
-    SECTION1.appendChild(p)
+    SECTION2.appendChild(p)
 }); 
-console.log(SECTION1);
-
-
-
 
 BODY.appendChild(HOME);
-
-//export { HOME }
