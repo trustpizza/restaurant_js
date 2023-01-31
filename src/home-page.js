6582c8c86aade41301f74361e62ef5ec4390bcb4
trustpizza <maxelolsson@gmail.com>
2023-01-30 19:04:25 -0600

    Begin to add page creation for home page logic

--- src/home-page.js
+++ src/home-page.js
@@ -1,6 +1,32 @@
-import { pageFactory } from './page-factory';
+import { pageFactory, divFactory } from './page-factory';
+import { header } from "./header";
+import _ from 'lodash';
+
+
+
+const HEADERBODY = header().container;
+const CONTAINER = document.getElementById("container");
+const HEADER = document.querySelector("header");
+HEADER.appendChild(HEADERBODY);
+
+// Body element to which ALL items will be added
 const BODY = document.getElementById("body");
+// Building the home page structure
 const HOME = pageFactory("main", 2).main;
+// Building first paragraph
+let page = []
+const SECTION1 = HOME.children[0].firstChild
+let title1 = document.createElement("h1");
+title1.textContent = "Hours of Operation"
+title1.classList.add("pl-4", "text-2xl", "font-medium", "underline");
+SECTION1.appendChild(title1);
+
+
+console.log(SECTION1);
+
+
+
+
 BODY.appendChild(HOME);
 
-export { HOME }
+//export { HOME }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import _ from 'lodash';
 import './style.css';
-import { HOME } from './home-page';
 
 // JS Files
-import { navbar } from './navbar';
-
-
-
-import { header } from "./header";
-const headerBody = header().container;
+import './navbar';
+import './home-page';
 
 // Building page
-const CONTAINER = document.getElementById("container");
-const HEADER = document.querySelector("header");
-HEADER.appendChild(headerBody);
+
 
 
