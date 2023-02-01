dd2bff9a24ebbba3e07613a8c59aeb2ca5d088d8
trustpizza <maxelolsson@gmail.com>
2023-02-01 16:05:32 -0600

    End for the day

--- src/menu-page.js
+++ src/menu-page.js
@@ -7,12 +7,16 @@ const menuPage = (container) => {
     const MENU = pageFactory("main", 1).main; // Creates Menu octagon items
      
     const SECTION1 = MENU.children[0].firstChild; // Selects the inner menu item to select the text section
+
+    // Title of the menu (this can be turned into its own function)
     let title = document.createElement("h1");
     title.textContent = "Menu";
     title.classList.add("text-2xl", "font-medium", "underline");
     SECTION1.appendChild(title);
 
-    BODY.appendChild(MENU);
+    // Menu Section (use simple paragraphs)
+
+    BODY.appendChild(MENU); // This moves the sections to the Body section
 }
 
 export { menuPage }
